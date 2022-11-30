c9403cef3b61aba971021a397f14c6d658df7239
riskinurhadi <rizkibinmangtrisno@gmail.com>
2022-11-30 10:52:48 +0700

    new file:   12.css 	new file:   12.html 	modified:   ALPEM.py 	modified:   KBMI11 	new file:   UU ITE.docx 	new file:   UU ITE.rar 	new file:   percProjwk.py 	new file:   ~$UU ITE.docx
    	modified:   KBMI11 (modified content, untracked content)

--- ALPEM.py
+++ ALPEM.py
@@ -3,14 +3,14 @@
 #Nilai dari list
 a = [1, -2, -8, -0, 3]
 print(a)
-print('Nilai Maksimum:', max(a))
+print('Nilai Maksimum=', max(a))
 
 #---------------------------------
 #2
 #nilai dari list
 a = [1, -2, -8, -0, 3]
 print(a)
-print('Nilai Minimum:', min(a))
+print('Nilai Minimum=', min(a))
 
 
 
